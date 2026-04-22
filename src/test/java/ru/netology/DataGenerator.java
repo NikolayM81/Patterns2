e8211c0569ee221ed58dcba4b30e235d16228033
Nikolay <mutovin36@gmail.com>
2026-04-22 12:22:07 +0300

    running the test1

---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,78 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(DataGenerator.RegistrationDto user) {
+
+        givin()
+                .spec(requestSpec)
+                .body(user)
+                .when().log().all()
+                .post("/api/system/users")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    private static RequestSpecification givin() {
+        return null;
+    }
+
+
+    public static String getRandomLogin() {
+
+        return faker.name().username();
+
+    }
+
+    public static String getRandomPassword() {
+
+        return faker.internet().password();
+
+    }
+
+    public static class Registration {
+        private Registration() {
+
+        }
+    }
+
+    public static RegistrationDto getUser(String status) {
+        return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+
+    }
+
+    public static RegistrationDto getRegisteredUser(String status) {
+        RegistrationDto user = getUser(status);
+        sendRequest(user);
+        return user;
+    }
+
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
